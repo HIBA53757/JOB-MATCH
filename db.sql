dbd64c4f5856bc3720bf0f77d19628e221cf39ed
HIBA53757 <hibatallahsadiki20@gmail.com>
2026-01-20 10:48:33 +0100

    Ignore node_modules

--- db.sql
+++ db.sql
@@ -12,3 +12,27 @@ CREATE TABLE users (
 );
 
 --@block
+CREATE TABLE `entreprise` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `secteur` VARCHAR(255),
+    `localisation` VARCHAR(255),
+    `email` VARCHAR(255),
+    `telephone` VARCHAR(50)
+);
+
+--@block
+CREATE TABLE `annonce` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT,
+    `typeContrat` VARCHAR(100),
+    `localisation` VARCHAR(255),
+    `image` VARCHAR(255),
+    `competences` TEXT,
+    `deleted` BOOLEAN DEFAULT FALSE,
+    `dateCreation` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `dateUpdate` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `entreprise_id` INT,
+    FOREIGN KEY (`entreprise_id`) REFERENCES `entreprise`(`id`) ON DELETE CASCADE
+);
