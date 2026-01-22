f8f65f960dfce73839e71891fa5f717e40064d26
mohamed_bouth <mohamedbouth87@gmail.com>
2026-01-22 09:10:42 +0100

    Dynamisez la page de l'annonce

--- app/controller/back/AnnonceController.php
+++ app/controller/back/AnnonceController.php
@@ -24,9 +24,18 @@ class AnnonceController extends baseController
     }
 
     public function renderPosts(){
+        $annonces = $this->annonce->findWithJoin("*" , "company" , 'annonce.company_id = company.id AND annonce.deleted = 0' , "INNER");
         if($this->session->get('user_role') !== "ADMIN"){
             $this->view->redirect('/login');
         }
-        $this->render("back/Posts", []);
+        $this->render("back/Posts", ["annonces" => $annonces]);
+    }
+
+    public function renderPostsArchived(){
+        $annonces = $this->annonce->findWithJoin("*" , "company" , 'annonce.company_id = company.id AND annonce.deleted = 1' , "INNER");
+        if($this->session->get('user_role') !== "ADMIN"){
+            $this->view->redirect('/login');
+        }
+        $this->render("back/Posts", ["annonces" => $annonces]);
     }
 }
